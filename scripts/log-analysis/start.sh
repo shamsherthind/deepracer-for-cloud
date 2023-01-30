f615faed79a301a60f66306437fd5980289fa42a
shamsherthind <ssthind.research@gmail.com>
2023-01-30 17:51:42 +0530

    Revert "allow log analysis to access minio server"
    This reverts commit 9bb7c8b169421920c7dc8ea6934821fdbb0710e7.

--- scripts/log-analysis/start.sh
+++ scripts/log-analysis/start.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 
 docker run --rm -d -p "8888:8888" \
--p "9000:9000" \
 -v `pwd`/../../data/logs:/workspace/logs \
 -v `pwd`/../../docker/volumes/.aws:/root/.aws \
 -v `pwd`/../../data/analysis:/workspace/analysis \
